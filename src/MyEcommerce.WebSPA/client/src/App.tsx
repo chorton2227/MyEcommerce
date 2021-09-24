399dc291137383d8bb3aebe5a54e1f987570bcf5
Chris Horton <chorton2227@gmail.com>
2021-09-24 13:23:50 -0400

    Better test for connecting to services

--- src/MyEcommerce.WebSPA/client/src/App.tsx
+++ src/MyEcommerce.WebSPA/client/src/App.tsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Configuration, ProductsApi } from 'typescript-axios-product-service';
-import logo from './logo.svg';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Configuration, ProductsApi, ProductReadDto } from 'typescript-axios-product-service';
 import './App.css';
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
   const [products, setProducts] = useState<any>({});
 
   useEffect(() => {
@@ -11,29 +15,28 @@ function App() {
   }, []);
 
   const getProducts = async () => {
-    const productsApiService = new ProductsApi(new Configuration({ basePath: "https://localhost:6001"}));
+    const productsApiService = new ProductsApi(new Configuration({ basePath: process.env.REACT_APP_PRODUCT_SERVICE_HOST }));
     const response = await productsApiService.getAll();
-    console.log(response);
-    setProducts(response);
+    setProducts(response.data);
+    setLoading(false);
   };
 
+  if (isLoading) {
+    return (
+      <div>
+        <ProgressBar striped variant="info" now={100} />
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        {products.map((product: ProductReadDto, index: any) => (
+          <Col key={index}>{product.name}</Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
