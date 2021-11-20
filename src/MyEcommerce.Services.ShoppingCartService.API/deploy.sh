276e4a921027162d98e9fc7704b0cc2643498415
Chris Horton <chorton2227@gmail.com>
2021-11-20 13:40:15 -0500

    Fix deploy

--- src/MyEcommerce.Services.ShoppingCartService.API/deploy.sh
+++ src/MyEcommerce.Services.ShoppingCartService.API/deploy.sh
@@ -8,5 +8,5 @@ docker push chorton2227/myecommerce.services.shoppingcartservice.api:$VERSION
 ssh root@chorton.dev "
     docker pull chorton2227/myecommerce.services.shoppingcartservice.api:$VERSION &&
     docker tag chorton2227/myecommerce.services.shoppingcartservice.api:$VERSION dokku/shopping-cart-service.myecommerce:latest &&
-    dokku deploy myecommerce latest
+    dokku deploy shopping-cart-service.myecommerce latest
 "
